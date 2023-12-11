e9756df6b7c1ce3df3c97089cb74d43f16612a1c
Amit Sharma <amitsharma@Amits-MacBook-Air.local>
2023-12-11 22:21:59 +0530

    seed users

--- backend/db/init_db.py
+++ backend/db/init_db.py
@@ -1,19 +1,16 @@
 from sqlmodel import Session, select, SQLModel
 from models import User, UserCreate
 from core.settings import settings
-import crud
+from crud.crud_user import create_user
 from db.engine import engine
+from api.endpoints.dto.usercreate import UserCreate
 
 
 def init_db(session: Session) -> None:
     SQLModel.metadata.create_all(bind=engine)
-    user = session.exec(select(User).where(
-        User.email == settings.FIRST_SUPERUSER))
-    if not user:
-        user_in = UserCreate(
-            email=settings.FIRST_SUPERUSER,
-            is_superuser=True,
-            password=settings.FIRST_SUPERUSER_PASSWORD
-        )
-
-        user = crud.create_user(session=session, user_create=user_in)
+    user = session.query(User).where(
+        User.email == settings.FIRST_SUPERUSER).all()
+    if len(user) == 0:
+        user_obj = UserCreate(email=settings.FIRST_SUPERUSER,
+                              password=settings.FIRST_SUPERUSER_PASSWORD, is_superuser=True)
+        user = create_user(session, user=user_obj)
